eeccc9837c7ebc735e095b4a5c98a8555023af8c
Skati <drozdovsv87@gmail.com>
2019-04-02 12:53:24 +0300

    change p4 palindromes

--- problems/p4.py
+++ problems/p4.py
@@ -1,10 +1,14 @@
-def pal():
+def pal(num=3):
     result=[]
-    for i in range(100,999):
-        for j in range(100,999):
+    largest=0
+    for i in range(10**(num-1),10**num):
+        for j in range(i,10**num):
             m=j*i
-            if str(m)==str(m)[::-1]:
-                result.append(str(m))
-    return max(result)
+            if str(m)==str(m)[::-1] and m>largest:
+                largest=i*j
+                i_=i
+                j_=j
+                result.append(m)
+    return largest,i_,j_
 
 print(pal())
